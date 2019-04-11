e77deea57161992136d75d6b290c3394102b4f65
Germoe <sebastian.engels@fulbrightmail.org>
2019-04-11 12:10:04 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project uses Billboard 200 and Spotify API data to make predictions on a song's potential to make it onto the Billboard 200 (become a mainstream hit).',
+    author='Sebastian Engels',
+    license='MIT',
+)
