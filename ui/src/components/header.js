f1be97fccddcb3f65a7dbb5b34da322f00b16101
Germoe <sebastian.engels@fulbrightmail.org>
2019-06-27 01:29:52 +0200

    update UI and add a Header

--- ui/src/components/header.js
+++ ui/src/components/header.js
@@ -1,14 +1,22 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+import mainImg from "../images/music_main_lg.jpg"
+
+const HeaderStyle = styled.header`
+  /*background-image: url("${mainImg}");
+  background-position: center;
+  background-size: cover;
+  box-shadow: inset 0 0 500px black;*/
+  background-color: #92319F;
+  min-height: 400px;
+  height: 40%;
+  margin-bottom: 1.45rem;
+`
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <HeaderStyle>
     <div
       style={{
         margin: `0 auto`,
@@ -16,19 +24,20 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+      <h1 style={{
+        margin: 0,
+        textAlign: `left`,
+        margin: `50px 0 30px 0`,
+        color: `white`,
+        textDecoration: `none`
+      }}>
+        Hit Predictor
       </h1>
+      <h4 style={{ textAlign: `left`, marginBottom: `100px` }}>
+        <em>by Sebastian Engels</em>
+      </h4>
     </div>
-  </header>
+  </HeaderStyle>
 )
 
 Header.propTypes = {
--- ui/src/pages/index.js
+++ ui/src/pages/index.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react"
 import { graphql } from "gatsby"
 import axios from "axios"
+import styled, { createGlobalStyle } from "styled-components"
+import AWS from "aws-sdk"
+import Header from "../components/header"
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Roboto');
+    font-family: 'Roboto', sans-serif;
+  }
+`
 
 // This query is executed at build time by Gatsby.
 // export const GatsbyQuery = graphql`
@@ -14,13 +24,90 @@ import axios from "axios"
 //   }
 // `
 
-import AWS from "aws-sdk"
+const SearchContainerStyle = styled.div`
+  width: 100%; 
+  text-align: center;
+  padding: 20px 10% 10% 10%;
+`
+
+const SearchInputStyle = styled.input`
+  min-width: 300px;
+  height: 60px;
+  padding: 0 100px;
+  margin: 10px 30px;
+  border-radius: 50px;
+
+  :focus {
+    outline: none;
+  }
+`
+
+const SearchButtonStyle = styled.button`
+  border-color: #92319F;
+  width: 200px;
+  height: 60px;
+  padding: 0 10px;
+  margin: 10px 30px;
+  border-radius: 50px;
+
+  :hover, :focus {
+    outline: none;
+    background-color: #92319F;
+    color: #FFFFFF;
+  }
+`
+
+const ButtonStyle = styled.button`
+  border-color: #92319F;
+  width: 200px;
+  height: 40px;
+  padding: 0 10px;
+  margin: 10px 30px;
+  border-radius: 50px;
+
+  :hover, :focus {
+    outline: none;
+    background-color: #92319F;
+    color: #FFFFFF;
+  }
+`
+
+const ListStyle = styled.div`
+  width: 95%;
+  max-width: 600px;
+  margin: 50px auto auto auto;
+`
+
+const SongContainerStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+`
+
+const AlbumImageContainerStyle = styled.div`
+  width: 30%;
+`
+
+const TextContainerStyle = styled.div`
+  width: 70%;
+`
+
 
 AWS.config.update({ region: 'us-east-2' });
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({ IdentityPoolId: 'us-east-2:28d93f6b-36ae-4739-afff-c7739b0a345e' });
 
+const Button = ({ loading, text, func }) => (
+  <>
+    {loading ? <ButtonStyle>Calculating ...</ButtonStyle> : <ButtonStyle onClick={func}>{text}</ButtonStyle>}
+  </>
+)
+
 class Predict extends Component {
   state = {
+    loading: false,
     hasPrediction: false,
     prediction: {},
     features: []
@@ -29,16 +116,16 @@ class Predict extends Component {
   render() {
     return (
       <div>
-        <button onClick={this.getPrediction}>Get Prediction</button>
         {this.state.hasPrediction ? <p>{this.state.prediction.hit ? 'Hit' : 'No Hit'}
           <br />
           Certainty: {Math.round(this.state.prediction.certainty * 10000) / 100}%
-        </p> : 'false'}
+        </p> : <Button loading={this.state.loading} text="Get Prediction" func={this.getPrediction} />}
       </div>
     )
   }
 
   getPrediction = () => {
+    this.setState({ loading: true })
     const sp = axios.create({
       baseURL: 'https://api.spotify.com/v1/',
       headers: {
@@ -56,6 +143,9 @@ class Predict extends Component {
           features.push(data[name])
         })
         this.scoreFeatures([features])
+      })
+      .catch(err => {
+        this.setState({ loading: false, err })
       })
   }
 
@@ -86,6 +176,7 @@ class Predict extends Component {
         pullResults = JSON.parse(data.Payload);
         const onePrediction = pullResults['values'][0]
         this.setState({
+          loading: false,
           hasPrediction: true,
           prediction: {
             hit: onePrediction[0],
@@ -98,14 +189,18 @@ class Predict extends Component {
 }
 
 const Song = ({ data, access_token }) => (
-  <div>
-    <img src={data.album.images[1].url} />
-    <p>{data.name}</p>
-    <Predict data={data} access_token={access_token} />
-  </div>
+  <SongContainerStyle>
+    <AlbumImageContainerStyle>
+      <img src={data.album.images[1].url} />
+    </AlbumImageContainerStyle>
+    <TextContainerStyle>
+      <p>{data.name}</p>
+      <Predict data={data} access_token={access_token} />
+    </TextContainerStyle>
+  </SongContainerStyle >
 )
 
-class ClientFetchingExample extends Component {
+class Main extends Component {
   state = {
     loading: false,
     q: 'Michael Jackson',
@@ -145,17 +240,20 @@ class ClientFetchingExample extends Component {
   render() {
     const items = this.state.items
     return (
-      <div style={{ textAlign: "center", width: "600px", margin: "50px auto" }}>
-        <div>
-          <input value={this.state.q} onChange={this.handleChange} />
-          <button onClick={this.fetchSpotifyCredentials}>Search</button>
-          {this.state.loading ? (
+      <>
+        <Header siteTitle="Predictions" />
+        <SearchContainerStyle>
+          <GlobalStyles />
+          <SearchInputStyle value={this.state.q} onChange={this.handleChange} />
+          <SearchButtonStyle onClick={this.fetchSpotifyCredentials}>Search</SearchButtonStyle>
+          <ListStyle>{this.state.loading ? (
             <p>Loading Songs</p>
           ) : items ? (
             items.map(data => <Song key={data.id} data={data} access_token={this.state.token.access_token} />)
           ) : null}
-        </div>
-      </div>
+          </ListStyle>
+        </SearchContainerStyle>
+      </>
     )
   }
 
@@ -218,4 +316,4 @@ class ClientFetchingExample extends Component {
   }
 }
 
-export default ClientFetchingExample
+export default Main
